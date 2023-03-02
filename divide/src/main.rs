19f59653db19d788da421dce868f55629a761b11
jkarrer <jkarrer@protect247.com>
2023-03-02 16:29:29 -0500

    :sparkles: divide floats

---
+++ divide/src/main.rs
@@ -0,0 +1,8 @@
+fn divide(dividend:f32, divisor:f32) -> f32 {
+  dividend / divisor
+}
+
+fn main() {
+    let quotient = divide(10.0, 5.0);
+    println!("{quotient}");
+}
---
+++ divide/target/debug/divide.d
@@ -0,0 +1,1 @@
+/Users/jkarrer/dev/projects/Personal/rust/utils/divide/target/debug/divide: /Users/jkarrer/dev/projects/Personal/rust/utils/divide/src/main.rs
